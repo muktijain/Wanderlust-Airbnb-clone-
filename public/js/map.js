efa62e06ae01bfce0dd80aa5c8baa08c196fbca7
Mukti Jain <05muktijain@gmail.com>
2025-11-19 23:10:35 +0530

    maps updated

--- public/js/map.js
+++ public/js/map.js
@@ -1,8 +1,9 @@
 mapboxgl.accessToken = mapToken;
 const map = new mapboxgl.Map({
-    container: 'map', // container ID
-    center: [-74.5, 40], // starting position [lng, lat]. Note that lat must be set between -90 and 90
-    zoom: 9 // starting zoom
+    container: 'map',
+    style: 'mapbox://styles/mapbox/streets-v12',   // REQUIRED
+    center: [-74.5, 40],
+    zoom: 9
 });
 
 
